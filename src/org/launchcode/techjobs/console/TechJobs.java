aa029f1c0aeb80ddeebb4fb287513e2960f2aadd
MikaZ21 <zukeyamamika@gmail.com>
2022-01-15 21:38:26 -0600

    finished assignment#1:Tech Jobs(Console Edition)

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -61,7 +61,8 @@ public class TechJobs {
                 String searchTerm = in.nextLine();
 
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not yet implemented.");
+                    printJobs((JobData.findByValue(searchTerm)));
+
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
@@ -111,6 +112,16 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.isEmpty()) {
+            System.out.println("No job found");
+    } else {
+            for (HashMap<String, String> aJob : someJobs) {
+                System.out.println("\n*********");
+                for (String jobKey : aJob.keySet()) {
+                    System.out.println(jobKey + " : " + aJob.get(jobKey));
+                }
+                System.out.println("*********");
+            }
+        }
     }
 }
